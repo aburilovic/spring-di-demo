f297b4275b8aab8a1f64163671e037274575c39a
Ante Burilovic <ante.burilovic.dev@gmail.com>
2019-08-27 15:27:40 +0200

    Spring DI simple example

--- src/main/java/com/aburilovic/springdidemo/SpringDiDemoApplication.java
+++ src/main/java/com/aburilovic/springdidemo/SpringDiDemoApplication.java
@@ -1,13 +1,18 @@
 package com.aburilovic.springdidemo;
 
+import com.aburilovic.springdidemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDiDemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SpringDiDemoApplication.class, args);
+		MyController controller = (MyController) ctx.getBean("myController");
+		controller.hello();
 	}
 
 }
